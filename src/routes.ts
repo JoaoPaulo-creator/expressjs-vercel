537e4bddcefd59d0e6d7c130ce82172814e537e1
JoaoPaulo-creator <joaosarto@gmail.com>
2023-07-11 22:51:54 -0300

    refactor: Add a redirect

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  return res.redirect("/teste");
+});
+
 router.get("/teste", (req, res) => {
   return res.json({ message: "Hello World!" });
 });
